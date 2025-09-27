c9b3a0dc439d8804a6df54d549b4a6e861103051
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2025-09-27 13:06:28 +0800

    upload activity 2

---
+++ app/src/main/java/com/example/multihello/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.multihello
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivityLifecycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        Log.d(tag, "onCreate called")
+
+        val btnNavigate: Button = findViewById(R.id.btnNavigate)
+        btnNavigate.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "onDestroy called")
+    }
+}
